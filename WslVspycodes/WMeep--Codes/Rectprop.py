f73663bc9a3cec0c40754e640dc33bb3968faede
SureshReddySangu <sangusureshreddy@gmail.com>
2024-09-06 15:50:25 +0200

    comitted tot tryat home

--- WslVspycodes/WMeep--Codes/Rectprop.py
+++ WslVspycodes/WMeep--Codes/Rectprop.py
@@ -59,13 +59,50 @@ K_0 = 0.5*(2*np.pi)/a #k_0 is the proapagation contstat in x direction
 Ez_list = []
 Ey_list = []
 Ex_list =[]
-t_values = np.linspace(0, 2*np.pi*a, 10)
-x_values = np.linspace(0, 4*2*a, 1*64)
-for i,t in enumerate(t_values):
-    for x in x_values:
-        Ez_list.append(Ez[64,:]*(np.exp(1j*K_0*x)*np.exp(1j*t)))
-        Ey_list.append(Ey[64,:]*(np.exp(1j*K_0*x)*np.exp(1j*t)))
-        Ex_list.append(Ex[64,:]*(np.exp(1j*K_0*x)*np.exp(1j*t)))
+period_inx = 4*2*a
+resl_inx = 2*64
+period_intime = 2*np.pi*a
+resl_intime =10
+t_values = np.linspace(0, 2*period_intime, resl_intime)
+x_values = np.linspace(0, period_inx*1, resl_inx)
+
+#time Evolution
+# for x in x_values:
+#     for t in t_values:
+#         Ez_list.append(Ez[64,:]*(np.exp(1j*K_0*x)*np.exp(1j*t)))
+#         Ey_list.append(Ey[64,:]*(np.exp(1j*K_0*x)*np.exp(1j*t)))
+#         Ex_list.append(Ex[64,:]*(np.exp(1j*K_0*x)*np.exp(1j*t)))
+# Ez_list = np.array(Ez_list)
+# Ey_list = np.array(Ey_list)
+# Ex_list = np.array(Ex_list)
+
+# Iz_list = np.abs(Ez_list)**2
+# Iy_list = np.abs(Ey_list)**2
+# Ix_list = np.abs(Ex_list)**2
+# I_tot_list = Iz_list + Iy_list +Ix_list
+# X, Y = np.meshgrid(128*np.linspace(0,1, 128), 640*np.linspace(0,1,640))
+# fig, ax  = plt.subplots()
+# Ezplt = ax.imshow(np.real(Ez_list), cmap= 'RdBu', aspect='equal')
+# ax.quiver(X,Y, np.real(Ex_list), color = 'green', scale = 10)
+# ax.set_title('propagating Ez filed ')
+# plt.colorbar(Ezplt, ax = ax)
+# # from matplotlib.animation import FuncAnimation
+# # def update(frame):
+# #     Ezplt.set_data(np.real(Ez_list[frame:]))
+# #     return[Ezplt]
+# # anim = FuncAnimation(fig, update, frames=(len(t_values)))
+# plt.show()
+
+
+
+
+
+
+# longitudanal
+for x in x_values:
+    Ez_list.append(Ez[64,:]*(np.exp(1j*K_0*x)))
+    Ey_list.append(Ey[64,:]*(np.exp(1j*K_0*x)))
+    Ex_list.append(Ex[64,:]*(np.exp(1j*K_0*x)))
 Ez_list = np.array(Ez_list)
 Ey_list = np.array(Ey_list)
 Ex_list = np.array(Ex_list)
@@ -74,14 +111,43 @@ Iz_list = np.abs(Ez_list)**2
 Iy_list = np.abs(Ey_list)**2
 Ix_list = np.abs(Ex_list)**2
 I_tot_list = Iz_list + Iy_list +Ix_list
+X, Y = np.meshgrid(resl_inx*np.linspace(0,1,resl_inx), 
+                   resl_inx*np.linspace(0,1, resl_inx))
+fig, ax  = plt.subplots(2,3)
+Ezplt_v = ax[0,0].imshow(np.real(Ez_list), cmap= 'RdBu', aspect='equal')
+ax[0,0].quiver(X, np.real(Ez_list), color = 'green', scale = 5)
+ax[0,0].set_title('propagating Ez filed ')
+plt.colorbar(Ezplt_v, ax = ax[0,0])
 
-fig, ax  = plt.subplots()
-Ezplt = ax.imshow(np.real(Ez_list), cmap= 'RdBu', aspect='equal')
-ax.set_title('propagating Ez filed ')
-plt.colorbar(Ezplt, ax = ax)
+Eyplt = ax[0,1].imshow(np.real(Ey_list), cmap= 'RdBu', aspect='equal')
+ax[0,1].quiver(X, np.real(Ey_list), color = 'green', scale = 15)
+ax[0,1].set_title('propagating Ey filed ')
+plt.colorbar(Eyplt, ax = ax[0,1])
+
+Explt = ax[1,0].imshow(np.real(Ex_list), cmap= 'RdBu', aspect='equal')
+ax[1,0].quiver(X, np.real(Ex_list), color = 'green', scale = 15)
+ax[1,0].set_title('propagating Ex filed ')
+plt.colorbar(Explt, ax = ax[1,0])
+
+Ezplt = ax[1,1].imshow(np.real(Ez_list), cmap= 'RdBu', aspect='equal')
+ax[1,1].set_title('propagating Ez filed ')
+plt.colorbar(Ezplt, ax = ax[1,1])
+
+Ezplt_ani = ax[0,2].imshow(np.real(Ez_list), cmap= 'RdBu', aspect='equal')
+ax[0,2].set_title('propagating Ez ani filed ')
+plt.colorbar(Ezplt_ani, ax = ax[0,2])
+
+Ezplt_aniV = ax[1,2].imshow(np.real(Ez_list), cmap= 'RdBu', aspect='equal')
+ax[1,2].quiver(X, np.real(Ez_list), color ='green', scale = 10)
+ax[1,2].set_title('propagating Ez V filed ')
+plt.colorbar(Ezplt_aniV, ax = ax[1,2])
+
 from matplotlib.animation import FuncAnimation
 def update(frame):
-    Ezplt.set_data(np.real(Ez_list[frame:]))
-    return[Ezplt]
+    Ezplt_ani.set_data(np.real(Ez_list[:frame]))
+    Ezplt_aniV.set_data(np.real(Ez_list[:frame]))
+    return[Ezplt_ani], [Ezplt_aniV]
 anim = FuncAnimation(fig, update, frames=(len(t_values)))
+
+plt.tight_layout()
 plt.show()
